cc9910a08d40f7ccf9c4675a819b11705a18e57f
Oleg Tiaglov <o.v.tiaglov@foxford.ru>
2025-01-22 16:18:25 +0100

    upd readme, test cases

--- src/index.ts
+++ src/index.ts
@@ -92,4 +92,68 @@ function checkResult(answer: AnswerType, correctAnswer: AnswerType) {
     return true;
 }
 
-// moved test cases to tests
+
+// duplicated in tests cases
+/**
+ * Пример1:
+ *
+ * users = [{ userId: "id1", score: 3 }, { userId: "id2", score: 2 }, { userId: "id3", score: 1 }]
+ * minScores = { firstPlaceMinScore: 100, secondPlaceMinScore: 50, thirdPlaceMinScore : 10 }
+ * Ожидаемый результат (любая сортировка внутри массива):
+ * [{ userId: "id1", place: 4 }, { userId: "id2", place: 5 }, { userId: "id3", place: 6 } ] // Все пользователи не набрали достаточно очков для 1, 2 и 3го места, поэтому заняли места с 4 по 6.
+ */
+let result1 = calculateLeaderboardPlaces(
+  [
+      { userId: "id1", score: 3 },
+      { userId: "id2", score: 2 },
+      { userId: "id3", score: 1 }
+  ],
+  { firstPlaceMinScore: 100, secondPlaceMinScore: 50, thirdPlaceMinScore: 10 }
+);
+console.log(
+  "test1",
+  checkResult(result1, [
+      { userId: "id1", place: 4 },
+      { userId: "id2", place: 5 },
+      { userId: "id3", place: 6 }
+  ])
+);
+
+/**
+ * Пример2:
+ *
+ * users = [{ userId: "id1", score: 100 }, { userId: "id2", score: 3 }, { userId: "id3", score: 2 }, { userId: "id4", score: 1 }]
+ * minScores = { firstPlaceMinScore: 100, secondPlaceMinScore: 50, thirdPlaceMinScore : 10 }
+ * Ожидаемый результат (любая сортировка внутри массива):
+ * [{ userId: "id1", place: 1 }, { userId: "id2", place: 4 }, { userId: "id3", place: 5 }, { userId: "id4", place: 6 }] // только "id1" набрал достаточно очков для 1го места, все остальные в топ не вошли
+ */
+let result2 = calculateLeaderboardPlaces(
+  [
+      { userId: "id1", score: 100 },
+      { userId: "id2", score: 3 },
+      { userId: "id3", score: 2 },
+      { userId: "id4", score: 1 }
+  ],
+  { firstPlaceMinScore: 100, secondPlaceMinScore: 50, thirdPlaceMinScore: 10 }
+);
+console.log(
+  "test2",
+  checkResult(result2, [
+      { userId: "id1", place: 1 },
+      { userId: "id2", place: 4 },
+      { userId: "id3", place: 5 },
+      { userId: "id4", place: 6 }
+  ])
+);
+
+/**
+ * Пример3
+ */
+let result3 = calculateLeaderboardPlaces([{ userId: "id1", score: 55 }], {
+    firstPlaceMinScore: 100,
+    secondPlaceMinScore: 50,
+    thirdPlaceMinScore: 10
+});
+console.log("test3", checkResult(result3, [{ userId: "id1", place: 2 }]));
+
+console.log("-----------------------------------------------------");
